2ac12bdfef4ee9633ffc0d64f8f05a58f31f8805
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-09-29 20:37:27 +0530

    components part 1

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Main from './components/MainComponent';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
