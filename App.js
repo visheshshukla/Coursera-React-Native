ef69c6caf3fa3525fe5e8a7fdab45eeaa7eb025a
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-10-04 02:35:34 +0530

    persist redux store

--- App.js
+++ App.js
@@ -3,14 +3,20 @@ import Main from './components/MainComponent';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate 
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
