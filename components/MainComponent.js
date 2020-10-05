d0ee637cdce1c6b1d73b5fa96a002cb0548a2bd7
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-10-06 02:34:37 +0530

    picking an image

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -166,7 +166,7 @@ const FavoritesNavigator = createStackNavigator({
 })
 
 const LoginNavigator = createStackNavigator({
-  Login: { screen: Login }
+  Login: Login
 }, {
 navigationOptions: ({ navigation }) => ({
   headerStyle: {
@@ -175,6 +175,7 @@ navigationOptions: ({ navigation }) => ({
   headerTitleStyle: {
       color: "#fff"            
   },
+  title: 'Login',
   headerTintColor: "#fff",
   headerLeft: <Icon name="menu" size={24}
     iconStyle={{ color: 'white' }} 
